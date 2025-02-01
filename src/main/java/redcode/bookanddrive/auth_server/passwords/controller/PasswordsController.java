0a2939bd98cd2a1d17fe0376cc66dd81a699f075
KUBA <kuba.piecuch.kp@gmail.com>
2025-02-01 16:13:19 +0100

    feat: BOO-17 review

--- src/main/java/redcode/bookanddrive/auth_server/passwords/controller/PasswordsController.java
+++ src/main/java/redcode/bookanddrive/auth_server/passwords/controller/PasswordsController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,7 +56,7 @@ public class PasswordsController {
             .build();
     }
 
-    @GetMapping("/reset")
+    @PostMapping("/reset")
     public ResponseEntity<String> resetPassword(
         @Valid @RequestBody PasswordResetRequest passwordResetRequest,
         @RequestParam("token") String oneTimeToken
